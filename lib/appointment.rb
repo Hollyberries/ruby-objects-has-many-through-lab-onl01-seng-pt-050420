f7647007278d3730dbdb8342573ad46ca4b8efc0
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-09 00:37:46 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,11 +4,12 @@ class Appointment
 
 attr_accessor :date, :patient, :doctor
 
-def initialize(date, patient, doctor)
+ def initialize(date, patient, doctor)
   @date = date
   @patient = patient
   @doctor = doctor
   @@all << self
+ end
   
   def self.all
     @@all
