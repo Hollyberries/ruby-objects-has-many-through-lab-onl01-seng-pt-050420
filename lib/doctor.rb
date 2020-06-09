d4ae56191d1ec9a2aeddc08e0ab499e25f88f781
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-09 00:47:39 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -21,8 +21,9 @@ class Doctor
     Appointment.new(date, patient, self)
   end
   
-  def patients
-    
+   def patients
+    appointments.collect {|appointment| appointment.patient}
+   end
   end
   
 end
