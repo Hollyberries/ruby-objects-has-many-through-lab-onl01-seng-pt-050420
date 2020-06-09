15653d191e5f68a6476504bb7509e9db65ec723d
Holly Miranda-Brennecke <loyaltothesport85@gmail.com>
2020-06-09 00:33:02 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,20 @@
+class Doctor
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date)
+    
+  end
+  
+end
